f670c7cbe843a0cbd8152f34020902c7f12c229c
revasta <85889757+revasta@users.noreply.github.com>
2021-08-01 21:35:57 +0530

    Card - Shared component

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+
+  @Input() hasImage = false;
+
+  @Input() hasLink = false;
+
+  @Input() title !: string;
+
+  @Input() content !: string;
+
+  @Input() imageUrl !: string;
+
+  @Input() linkUrl !: string;
+
+  @Input() linkName !: string;
+
+  @Input() height = 300;
 
   constructor() { }
 
